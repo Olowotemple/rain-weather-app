e4f1536c76afc8b9a70627da9dad2d1a803a8df6
Temple Olowonigba <olowotemple@yahoo.com>
2021-10-19 16:36:40 +0100

    Add offline capability for the favourites list

--- src/contexts/FavouriteContext.js
+++ src/contexts/FavouriteContext.js
@@ -1,17 +1,22 @@
 import { createContext, useContext, useState } from 'react';
+import { readFromStorage, saveToStorage } from '../utils/helperFunctions';
 
 const FavouriteContext = createContext(null);
 
 export const FavouriteContextProvider = ({ children }) => {
-  const [favourites, setFavourites] = useState([]);
+  const [favourites, setFavourites] = useState(
+    readFromStorage('favourites') || []
+  );
 
   const addToFavourite = (name) => {
     const favourite = [...favourites, name];
+    saveToStorage('favourite', favourite);
     setFavourites(favourite);
   };
 
   const removeFromFavourite = (name) => {
     const filteredFavourites = favourites.filter((fav) => fav !== name);
+    saveToStorage('favourites', filteredFavourites);
     setFavourites(filteredFavourites);
   };
 
